1c8d4028071b8c736cf46a6325914d0b05eb3326
Danil Khairulin <enddonil@gmail.com>
2026-01-23 11:51:37 +0300

    replace paddings with spacer

--- creator/src/main/java/com/example/creator/ExampleScreen.kt
+++ creator/src/main/java/com/example/creator/ExampleScreen.kt
@@ -14,8 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.remote.creation.compose.action.Action
 import androidx.compose.remote.creation.compose.action.HostAction
 import androidx.compose.remote.creation.compose.capture.captureSingleRemoteDocument
+import androidx.compose.remote.creation.compose.layout.RemoteAlignment
+import androidx.compose.remote.creation.compose.layout.RemoteArrangement
+import androidx.compose.remote.creation.compose.layout.RemoteBox
 import androidx.compose.remote.creation.compose.layout.RemoteColumn
 import androidx.compose.remote.creation.compose.layout.RemoteComposable
+import androidx.compose.remote.creation.compose.layout.RemotePaddingValues
 import androidx.compose.remote.creation.compose.layout.RemoteRow
 import androidx.compose.remote.creation.compose.layout.RemoteText
 import androidx.compose.remote.creation.compose.modifier.RemoteModifier
@@ -27,6 +31,7 @@ import androidx.compose.remote.creation.compose.modifier.size
 import androidx.compose.remote.creation.compose.state.RemoteDp
 import androidx.compose.remote.creation.compose.state.RemoteFloat
 import androidx.compose.remote.creation.compose.state.rdp
+import androidx.compose.remote.creation.compose.state.rf
 import androidx.compose.remote.creation.compose.state.ri
 import androidx.compose.remote.creation.compose.state.rs
 import androidx.compose.remote.creation.compose.v2.RemoteColumnV2
@@ -61,16 +66,20 @@ object ExampleScreen {
                 .fillMaxSize(),
         ) {
             RemoteText(text = "Hello Remote Compose")
+            RemoteBox(modifier = RemoteModifier.size(12.rdp))
             repeat(3) {
                 RemoteRow(
                     modifier = RemoteModifier
-                        .padding(12.dp)
+                        .size(50.rdp)
                         .background(color = Color.White)
                         .clickable(HostAction("value".rs,it.ri))
-                        .padding(12.dp)
+                    ,
+                    verticalAlignment = RemoteAlignment.CenterVertically,
+                    horizontalArrangement = RemoteArrangement.CenterHorizontally
                 ) {
                     RemoteText(text = "$it")
                 }
+                RemoteBox(modifier = RemoteModifier.size(12.rdp))
             }
         }
     }
