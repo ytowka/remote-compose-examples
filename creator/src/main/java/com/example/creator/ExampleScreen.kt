e638dd4195ba342e110e361320efd905c5f8289f
Danil Khairulin <enddonil@gmail.com>
2026-01-29 16:58:01 +0300

    functions

--- creator/src/main/java/com/example/creator/ExampleScreen.kt
+++ creator/src/main/java/com/example/creator/ExampleScreen.kt
@@ -2,6 +2,7 @@ package com.example.creator
 
 import android.annotation.SuppressLint
 import android.content.Context
+import androidx.compose.remote.creation.compose.action.Action
 import androidx.compose.remote.creation.compose.action.HostAction
 import androidx.compose.remote.creation.compose.action.ValueChange
 import androidx.compose.remote.creation.compose.capture.captureSingleRemoteDocument
@@ -12,12 +13,17 @@ import androidx.compose.remote.creation.compose.layout.RemoteColumn
 import androidx.compose.remote.creation.compose.layout.RemoteComposable
 import androidx.compose.remote.creation.compose.layout.RemoteRow
 import androidx.compose.remote.creation.compose.layout.RemoteText
+import androidx.compose.remote.creation.compose.layout.rememberRemoteStringList
 import androidx.compose.remote.creation.compose.modifier.RemoteModifier
 import androidx.compose.remote.creation.compose.modifier.background
 import androidx.compose.remote.creation.compose.modifier.clickable
 import androidx.compose.remote.creation.compose.modifier.fillMaxSize
+import androidx.compose.remote.creation.compose.modifier.height
 import androidx.compose.remote.creation.compose.modifier.size
+import androidx.compose.remote.creation.compose.modifier.toComposeUi
 import androidx.compose.remote.creation.compose.state.MutableRemoteInt
+import androidx.compose.remote.creation.compose.state.RemoteDp
+import androidx.compose.remote.creation.compose.state.RemoteString
 import androidx.compose.remote.creation.compose.state.rdp
 import androidx.compose.remote.creation.compose.state.rememberMutableRemoteFloat
 import androidx.compose.remote.creation.compose.state.rememberRemoteInt
@@ -48,9 +54,9 @@ object ExampleScreen {
     @Composable
     @RemoteComposable
     fun RemoteScreen() {
-        val intState = remember { MutableRemoteInt(0) }
 
         val stringState = rememberRemoteString { "start" }
+        val listState = rememberRemoteStringList("first", "second")
 
         RemoteColumn(
             modifier = RemoteModifier
@@ -58,24 +64,56 @@ object ExampleScreen {
                 .fillMaxSize(),
         ) {
             RemoteText(text = "Hello Remote Compose")
-            RemoteBox(modifier = RemoteModifier.size(12.rdp))
+            RemoteSpacer(12.rdp)
             repeat(3) {
-                RemoteRow(
-                    modifier = RemoteModifier
-                        .size(50.rdp)
-                        .background(color = Color.White)
-                        //.clickable(HostAction("value".rs,it.ri))
-                        .clickable(ValueChange(stringState, "end"))
-                    ,
-                    verticalAlignment = RemoteAlignment.CenterVertically,
-                    horizontalArrangement = RemoteArrangement.CenterHorizontally
-                ) {
-                    RemoteText(text = "$it")
-                }
-                RemoteBox(modifier = RemoteModifier.size(12.rdp))
-                RemoteText(text = stringState)
+                RemoteButton(
+                    text = it.ri.toRemoteString(10),
+                    ValueChange(stringState, "end")
+                )
+                RemoteSpacer(12.rdp)
             }
+            RemoteText(text = stringState)
+            /*if(stringState == "end".rs) {
+                RemoteText(text = "updated")
+            }*/
         }
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Composable
+    @RemoteComposable
+    fun RemoteButton(
+        text: RemoteString,
+        vararg actions: Action
+    ) {
+        RemoteRow(
+            modifier = RemoteModifier
+                .background(color = Color.White)
+                .clickable(actions = actions)
+            ,
+            verticalAlignment = RemoteAlignment.CenterVertically,
+            horizontalArrangement = RemoteArrangement.CenterHorizontally
+        ) {
+            
+            RemoteSpacer(12.rdp)
+            RemoteColumn(
+                horizontalAlignment = RemoteAlignment.CenterHorizontally,
+                verticalArrangement = RemoteArrangement.Center
+            ) {
+
+                RemoteSpacer(12.rdp)
+                RemoteText(text = text)
+                RemoteSpacer(12.rdp)
+            }
+            RemoteSpacer(12.rdp)
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Composable
+    @RemoteComposable
+    fun RemoteSpacer(size: RemoteDp) {
+        RemoteBox(modifier = RemoteModifier.size(size))
     }
 }
 
